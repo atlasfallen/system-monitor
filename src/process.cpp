1b2b0dba076c127e8f2fdee4b3652f82f2d0350a
atlasfallen <sendalex@me.com>
2020-11-05 03:04:51 +0000

    implement Process::Command()

--- src/process.cpp
+++ src/process.cpp
@@ -1,4 +1,5 @@
 #include "process.h"
+#include "linux_parser.h"
 
 #include <unistd.h>
 
@@ -20,10 +21,11 @@ int Process::Pid() { return pid_; }
 float Process::CpuUtilization() { return 0; }
 
 // TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+string Process::Command() { return LinuxParser::Command(pid_); }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+string Process::Ram() { return string();}
+// string Process::Ram() { return LinuxParser::Ram(pid_); } //TODO: Ram crashes app
 
 // TODO: Return the user (name) that generated this process
 string Process::User() { return string(); }
