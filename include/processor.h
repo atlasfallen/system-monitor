cc8f2bf498fbef34b7c19924a4e226fa019c4d12
atlasfallen <sendalex@me.com>
2020-11-05 14:23:55 +0000

    refactored Processor::Utilization()

--- include/processor.h
+++ include/processor.h
@@ -3,13 +3,12 @@
 
 class Processor {
  public:
-  float Utilization();  // TODO: See src/processor.cpp
+  float Utilization();
 
-  // TODO: Declare any necessary private members
+  // Declare any necessary private members
  private:
-  // long uInterval_{1000}; // microsecond interval
-  long previous_active_system_ticks_;
-  long previous_total_system_ticks_;
+  long active_system_ticks_;
+  long total_system_ticks_;
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -4,16 +4,9 @@
 
 #include "linux_parser.h"
 
-// TODO: Return the aggregate CPU utilization
+// Return the aggregate CPU utilization
 float Processor::Utilization() {
-  long previous_active_system_ticks_ = LinuxParser::ActiveJiffies();
-  long previous_total_system_ticks_ = LinuxParser::Jiffies();
-
-//   usleep(uInterval_);
-
-  // long next_active_system_ticks = LinuxParser::ActiveJiffies();
-  // long next_total_system_ticks = LinuxParser::Jiffies();
-
-  return (float)previous_active_system_ticks_ /
-         (float)previous_total_system_ticks_;
+  active_system_ticks_ = LinuxParser::ActiveJiffies();
+  total_system_ticks_ = LinuxParser::Jiffies();
+  return (float)active_system_ticks_ / (float)total_system_ticks_;
 }
