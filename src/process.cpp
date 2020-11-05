076bd324091fb4cb19fc738a4b096aa3b25f967c
atlasfallen <sendalex@me.com>
2020-11-05 03:06:58 +0000

    implement Process::User()

--- src/process.cpp
+++ src/process.cpp
@@ -20,15 +20,15 @@ int Process::Pid() { return pid_; }
 // TODO: Return this process's CPU utilization
 float Process::CpuUtilization() { return 0; }
 
-// TODO: Return the command that generated this process
+// DONE: Return the command that generated this process
 string Process::Command() { return LinuxParser::Command(pid_); }
 
 // TODO: Return this process's memory utilization
 string Process::Ram() { return string();}
 // string Process::Ram() { return LinuxParser::Ram(pid_); } //TODO: Ram crashes app
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+// DONE: Return the user (name) that generated this process
+string Process::User() { return LinuxParser::User(pid_); }
 
 // TODO: Return the age of this process (in seconds)
 long int Process::UpTime() { return 0; }
